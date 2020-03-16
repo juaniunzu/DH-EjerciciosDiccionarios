b86f472f97b51f3d6729e3b001cce50f049054fa
Juani Unzurrunzaga <juanunzu@gmail.com>
2020-03-16 16:53:17 -0300

    Terminado

---
+++ src/com/company/Main.java
@@ -0,0 +1,72 @@
+package com.company;
+
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        /* ejercicio 1
+
+        Map<Integer, String> quiniela = new HashMap<Integer, String>();
+
+        quiniela.put(0, "huevos");
+        quiniela.put(1, "agua");
+        quiniela.put(2, "escopeta");
+        quiniela.put(3, "caballo");
+        quiniela.put(4, "dentista");
+        quiniela.put(5, "llamas");
+
+        for (Integer integer : quiniela.keySet()) {
+            System.out.println(integer);
+            System.out.println(quiniela.get(integer));
+        }
+
+         */
+
+        /* ejercicio 2
+
+
+        Map<String, List<String>> amigosYApodos = new HashMap<String, List<String>>();
+
+        List<String> apodosJuan = new ArrayList<String>();
+        apodosJuan.add("Juancho");
+        apodosJuan.add("Fisura");
+        apodosJuan.add("ricardo fort");
+
+        List<String> apodosMiguel = new ArrayList<String>();
+        apodosMiguel.add("Night watch");
+        apodosMiguel.add("bruce wayne");
+        apodosMiguel.add("pati corti");
+
+        List<String> apodosMaria = new ArrayList<String>();
+        apodosMaria.add("wonder woman");
+        apodosMaria.add("mary");
+        apodosMaria.add("roberto");
+
+        List<String> apodosLucas = new ArrayList<String>();
+        apodosLucas.add("luks");
+        apodosLucas.add("jorge");
+        apodosLucas.add("chorch");
+
+        amigosYApodos.put("Juan", apodosJuan);
+        amigosYApodos.put("Miguel", apodosMiguel);
+        amigosYApodos.put("Maria", apodosMaria);
+        amigosYApodos.put("Lucas", apodosLucas);
+
+        for (String s : amigosYApodos.keySet()) {
+            System.out.println(s);
+            System.out.println(amigosYApodos.get(s));
+        }
+
+         */
+
+
+
+
+    }
+}
